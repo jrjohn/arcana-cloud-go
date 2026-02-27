f026ff6142309a285cb9ddee610fd1277ce41498
Arcana <arcana@arcana.boo>
2026-02-27 10:08:45 +0000

    refactor: add input validation to TruncateString

--- internal/utils/string_utils.go
+++ internal/utils/string_utils.go
@@ -2,15 +2,19 @@ package utils
 
 import "strings"
 
-// TruncateString truncates a string to maxLen characters
+// TruncateString truncates a string to maxLen characters.
+// Returns original string if shorter than maxLen.
 func TruncateString(s string, maxLen int) string {
+    if maxLen <= 0 {
+        return ""
+    }
     if len(s) <= maxLen {
         return s
     }
     return s[:maxLen] + "..."
 }
 
-// ContainsAny returns true if s contains any of the substrings
+// ContainsAny returns true if s contains any of the substrings.
 func ContainsAny(s string, substrings []string) bool {
     for _, sub := range substrings {
         if strings.Contains(s, sub) {
